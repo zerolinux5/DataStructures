bcf6e0496b415210d6e9ed0664d9a160f4d6f88b
Jesus Magana <maganazero5@gmail.com>
2017-05-27 07:45:57 -0700

    feat: Add node class.
    Used integer as a base type.
    Haven't tested implementation yet.

---
+++ node.cpp
@@ -0,0 +1,39 @@
+/*
+ * Node class implementation.
+ */
+
+#include <iostream>
+#include <cstddef>
+#include "node.h"
+
+
+/* Default constructor. Set data to 0 and next to NULL. */
+Node::Node(){
+	data = 0;
+	next = NULL;
+}
+
+/* Check if node points to another Node. */
+bool Node::hasNext(){
+	return (next == NULL) ? false : true;
+}
+
+/* Return next node. */
+Node *Node::getNext(){
+	return next;
+}
+
+/* Set next node. */
+void Node::setNext(Node *nextNode) {
+	next = nextNode;
+}
+
+/* Get value of data. */
+int Node::getValue(){
+	return data;
+}
+
+/* Set data value. */
+void Node::setValue(int newData){
+	data = newData;
+}
---
+++ node.h
@@ -0,0 +1,20 @@
+/*
+ * Node class header file.
+ */
+
+#ifndef __NODE_H__
+#define __NODE_H__
+
+class Node {
+	int data;
+	Node *next;
+public:
+	Node();
+	bool hasNext();
+	Node *getNext();
+	void setNext(Node *nextNode);
+	int getValue();
+	void setValue(int newData);
+};
+
+#endif
